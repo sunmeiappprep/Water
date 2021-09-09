3d79c22d0b15f3fd24c144f016f6bcc6a99f639f
Sunny Mei <sunmeiappprep@gmail.com>
2021-09-09 09:20:14 -0400

    breaking upper and lower text in card

--- frontend/components/card/card.jsx
+++ frontend/components/card/card.jsx
@@ -4,13 +4,17 @@ import Cardsmall from './cardsmall.jpg'
 class Card extends React.Component {
 
     render() {
-        const text = 'Hawaii \n 7.5 Hour Flight';
+        const text = 'Hawaii \n ';
+        const text2 = '7.5 Hour Drive'
         return (
             <div className="card">
                 <img className='card2' src={Cardsmall} alt="cardsmall" />
-                <p className="display-linebreak"> 
-                    {text} 
+                <p> 
+                    <section className="upperText">{text} </section>
+                    <section className="lowerText"> {text2}</section>
+                   
                 </p>
+                
                
 
             </div>
