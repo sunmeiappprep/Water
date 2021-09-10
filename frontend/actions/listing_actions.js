f63f0f663268b18de4ee6d4455d8cced79f94940
Sunny Mei <sunmeiappprep@gmail.com>
2021-09-10 13:43:24 -0400

    listing actions

---
+++ frontend/actions/listing_actions.js
@@ -0,0 +1,30 @@
+import * as ListingUtils from '../util/listing_utils';
+
+export const RECEIVE_LISTINGS = "RECEIVE_LISTINGS";
+export const RECEIVE_LISTING = "RECEIVE_LISTING";
+
+export const receiveListings = listings => ({
+    type: RECEIVE_LISTINGS,
+    listings 
+})
+
+export const receiveListing = listing => ({
+    type: RECEIVE_LISTING,
+    listing 
+})
+
+
+export const fetchListings = () => dispatch => {
+    return(
+    ListingUtils.fetchListings()
+    .then(listings => dispatch(receiveListings(listings)))
+    )
+};
+
+export const fetchListing = listingid => dispatch => {
+    return(
+    ListingUtils.fetchListing(listingid)
+    .then(listing => dispatch(receiveListing(listing)))
+    )
+};
+
