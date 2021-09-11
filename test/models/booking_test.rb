24104b55e9a982ce7de452305f396a76586803fd
Sunny Mei <sunmeiappprep@gmail.com>
2021-09-11 16:40:30 -0400

    annotated

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id         :bigint           not null, primary key
+#  check_in   :date             not null
+#  check_out  :date             not null
+#  renter_id  :integer          not null
+#  listing_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class BookingTest < ActiveSupport::TestCase
